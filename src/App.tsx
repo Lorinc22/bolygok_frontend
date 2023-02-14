2ae6b532861751f2812ccbffc0d714786c5bf356
Lorinc22 <krity.lorinc@gmail.com>
2023-02-14 11:01:51 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+
+
+
+interface State{
+  bolygok: Bolygo[];
+  nev: string;
+  nev2: string;
+  atmero: number
 }
 
+interface Bolygo{
+  id: number;
+  nev: string;
+  nev2: string;
+  atmero: number;
+}
+
+class App extends Component<{}, State>{
+
+  constructor(props: {}){
+    super(props);
+
+    this.state = {
+      nev: "",
+      nev2: "",
+      atmero: 0,
+      bolygok: [],
+    }
+  }
+
+
+  async loadBolygok() {
+    let respone = await fetch('http://localhost:3000/bolygo');
+    let data = await respone.json() as Bolygo[];
+    this.setState({
+      bolygok: data
+    })
+  }
+
+  componentDidMount() {
+    this.loadBolygok();
+  }
+
+  handleUpload = async () => {
+    const { nev, nev2, atmero } = this.state;
+    if(nev.trim() === '' || nev2.trim() === '' || atmero <1){
+      return;
+    }
+
+    const adat = {
+      nev: nev,
+      nev2: nev2,
+      atmero: atmero,
+    }
+
+    let response = await fetch('http://localhost:3000/bolygo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(adat),
+    });
+
+    this.setState({ 
+      nev: '',
+      nev2: '',
+      atmero: 0,
+    })
+
+    await this.loadBolygok();
+  };
+
+  async handleDelete(id: number) {
+    let respone = await fetch('http://localhost:3000/bolygo/ '+ id,{
+      method: 'DELETE',
+    }
+    
+    
+    )
+    await this.loadBolygok();
+  }
+
+
+render() {
+  return <div className='div'>
+    <h2>Bolygok</h2>
+    Név: <input type="text" value={this.state.nev} onChange={e => this.setState({ nev: e.currentTarget.value})}/><br />
+    Név2: <input type="text" value={this.state.nev2} onChange={e => this.setState({ nev2: e.currentTarget.value})} /><br />
+    Atmero: <input type="number" value={this.state.atmero} onChange={e => this.setState({ atmero: parseInt(e.currentTarget.value)})}/><br />
+    <button onClick={this.handleUpload}>Hozzáad</button> <br />
+   
+   
+  </div>
+}
+}
 export default App;
